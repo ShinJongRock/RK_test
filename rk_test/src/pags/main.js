813e5dcedc5201f9774062baf63a405ac877aeaa
sjr0630@gmail.com <whdfkr0630@gmail.com>
2023-04-04 11:33:42 +0900

    제목

--- rk_test/src/pags/main.js
+++ rk_test/src/pags/main.js
@@ -174,7 +174,7 @@ export default function PrimarySearchAppBar() {
                         component="div"
                         sx={{ display: { xs: 'none', sm: 'block' } }}
                     >
-                        MUI
+                        종락
                     </Typography>
                   
                         <Search
